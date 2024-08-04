2027d839b72b9448f060b81a5ea6243ad452d240
srividyadonthineni <s.donthineni@ufl.edu>
2024-08-04 16:25:47 -0400

    Hashmap Function - Retrieves all songs from the chosen year and place into a map using the release date as the key and song name as the value to help with sorting.

--- FrontEnd.cpp
+++ FrontEnd.cpp
@@ -2,38 +2,32 @@
 #include <fstream>
 #include <iostream>
 #include <SFML/Graphics.hpp>
+
 using namespace std;
 
 void SetText(sf::Text &text, float x, float y) {
     sf::FloatRect textRect = text.getLocalBounds();
-    text.setOrigin(textRect.left + textRect.width/2.0f,
-                   textRect.top + textRect.height/2.0f);
+    text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
     text.setPosition(sf::Vector2f(x, y));
 }
 
 int CreateHomeScreen() {
     int width = 1024;
     int height = 768;
-    // Creates window
     sf::RenderWindow window(sf::VideoMode(width, height), "GATOR POP");
-    // Gets necessary font
     sf::Font welcome_font;
 
-    // Creates welcome text
     sf::Text welcome_text("WELCOME TO GATOR POP!", welcome_font, 60);
     welcome_text.setFillColor(sf::Color::Black);
     welcome_text.setStyle(sf::Text::Bold);
     SetText(welcome_text, 125, 100);
 
-    // Button Sprites
     sf::Texture buttonTexture;
     if (!buttonTexture.loadFromFile("files/images/Generate Button.png")) {
-        // Handle loading error
         return -1;
     }
 
-    if (!welcome_font.loadFromFile(
-            "files/font.ttf")) {
+    if (!welcome_font.loadFromFile("files/font.ttf")) {
         cout << "not working" << endl;
     }
 
@@ -44,50 +38,42 @@ int CreateHomeScreen() {
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            // Dev shortcut, push key to close
             if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed) {
                 window.close();
             }
-            if (buttonSprite.getGlobalBounds().contains({(float) event.mouseButton.x, (float) event.mouseButton.y})) {
-                CreateChart(welcome_font);
-//                cout << "Button Clicked" << endl;
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (buttonSprite.getGlobalBounds().contains({ (float)event.mouseButton.x, (float)event.mouseButton.y })) {
+                    CreateChart(welcome_font);
+                }
             }
         }
 
-
-
-
-        // Window color and text
         window.clear(sf::Color(208, 240, 192));
         window.draw(welcome_text);
-
-
         window.draw(buttonSprite);
-
         window.display();
     }
+
+    return 0;
 }
 
-void CreateChart(sf::Font welcome_font)
-{
-
-    sf::RenderWindow leaderboard_window(sf::VideoMode(1024,768), "Playlist");
+void CreateChart(sf::Font welcome_font) {
+    sf::RenderWindow leaderboard_window(sf::VideoMode(1024, 768), "Playlist");
 
     sf::Text leaderboard_text("Songs Ranked By Release Date", welcome_font, 20);
     leaderboard_text.setFillColor(sf::Color::Black);
     leaderboard_text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-    SetText(leaderboard_text, 512, 100);
+    SetText(leaderboard_text, 512, 50);
 
     string all_names;
-    vector<string> leaderboard_vec;
-    leaderboard_vec = ReadFile();
+    vector<string> leaderboard_vec = ReadFile();
     for (int i = 0; i < leaderboard_vec.size() - 2; i += 2) {
         int num_place = i / 2 + 1;
-        if(num_place < 10){
-            all_names += to_string(num_place) + ".\     " + leaderboard_vec[i] + "\t" + leaderboard_vec[i + 1] + "\n\n";
+        if (num_place < 10) {
+            all_names += to_string(num_place) + ".     " + leaderboard_vec[i] + "\t" + leaderboard_vec[i + 1] + "\n\n";
         }
-        else{
-            all_names += to_string(num_place) + ".\    " + leaderboard_vec[i] + "\t" + leaderboard_vec[i + 1] + "\n\n";
+        else {
+            all_names += to_string(num_place) + ".    " + leaderboard_vec[i] + "\t" + leaderboard_vec[i + 1] + "\n\n";
         }
     }
 
@@ -97,67 +83,62 @@ void CreateChart(sf::Font welcome_font)
     player_data.setCharacterSize(18);
     player_data.setFillColor(sf::Color::Black);
     player_data.setStyle(sf::Text::Bold);
-    SetText(player_data, 512, 400);
+    player_data.setPosition(512, 150); // Adjust initial position
 
+    sf::View view = leaderboard_window.getView();
+    float scroll_speed = 50.0f;
 
     while (leaderboard_window.isOpen()) {
         sf::Event event;
         while (leaderboard_window.pollEvent(event)) {
-            // Short cut
             if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed) {
                 leaderboard_window.close();
             }
-            if (event.type == sf::Event::MouseButtonPressed) {
+            if (event.type == sf::Event::MouseWheelScrolled) {
+                if (event.mouseWheelScroll.delta > 0) {
+                    view.move(0, -scroll_speed);
+                }
+                else {
+                    view.move(0, scroll_speed);
+                }
+                leaderboard_window.setView(view);
             }
         }
-        // colors light grey = 211, 211, 211
+
         leaderboard_window.clear(sf::Color(211, 211, 211));
         leaderboard_window.draw(leaderboard_text);
         leaderboard_window.draw(player_data);
         leaderboard_window.display();
-
     }
 }
 
 vector<string> ReadFile() {
-    ifstream readleaderboard;
-    readleaderboard.open("files/leaderboard.txt");
-
+    ifstream readleaderboard("files/leaderboard.txt");
     string temp;
     vector<string> leaderboard_vec;
-    while(readleaderboard) {
-        getline(readleaderboard, temp, ',');
+    while (getline(readleaderboard, temp, ',')) {
         leaderboard_vec.push_back(temp);
         getline(readleaderboard, temp, '\n');
         leaderboard_vec.push_back(temp);
     }
-    readleaderboard.close();
     return leaderboard_vec;
 }
 
 void WriteToFile(vector<pair<string, string>> &song_data) {
-    ofstream write_to_file;
+    ofstream write_to_file("files/leaderboard.txt");
     int size = song_data.size();
-    // Calculate start_index to avoid negative index
-    int start_index = 0;
-    if (size > 50) {
-        start_index = size - 50;
-    }
-    write_to_file.open("files/leaderboard.txt");
-    for(int i = size - 1; i >= start_index; --i) {
+    int start_index = max(0, size - 50);
+    for (int i = size - 1; i >= start_index; --i) {
         write_to_file << song_data[i].first << ", " << song_data[i].second << "\n";
     }
-    write_to_file.close();
 }
 
 vector<pair<string, string>> ConvertVec(vector<pair<int, string>> &raw_data) {
     vector<pair<string, string>> song_data;
-    for(int i = 0; i < raw_data.size(); i++){
-// Using to see if it will write data, we need to find a way to write our top 10 spotify data
-        std::string date = to_string(raw_data[i].first);
-        std::string song = raw_data[i].second;
-        song_data.push_back({date, song});
+    for (const auto &data : raw_data) {
+        string date = to_string(data.first);
+        string song = data.second;
+        song_data.push_back({ date, song });
     }
     return song_data;
 }
-
